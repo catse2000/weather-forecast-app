32b451139d3a8658756579265fdbc39206bc6b82
catse2000 <catse2000@yahoo.com>
2020-09-07 01:06:22 -0500

    add uv index and create if statement to determine color the value should be

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var searchInputEl = document.querySelector("#search-input");
 
 var getWeather = function(city) {
     //format the weather link
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=9e1fdaa540fdf17e456714b519cc5694";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=9e1fdaa540fdf17e456714b519cc5694";
     
     //make a request to the url
     fetch(apiUrl).then(function(response){
@@ -54,10 +54,56 @@ var displayWeather = function(info, searchTerm){
     var presentTemp = document.querySelector("#forecast-present-temp");
     var presentHumidity = document.querySelector("#forecast-present-humidity");
     var presentWindSpeed = document.querySelector("#forecast-present-speed");
-    var presentUVIndex = document.querySelector("#forecast-present-uv");
-    console.log(info);
+    
+    presentTemp.innerHTML = info.main.temp + " &#176;";
+    presentHumidity.textContent = info.main.humidity + " %";
+    presentWindSpeed.textContent = info.wind.speed + " MPH";
+    getUVIndex(info); 
     
 
 };
 
+var getUVIndex = function (cityData){
+    var long = cityData.coord.lon;
+    var lat = cityData.coord.lat;
+
+    var url = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + long + "&appid=9e1fdaa540fdf17e456714b519cc5694&";
+
+    //make a request to the url
+    fetch(url).then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                var uv = data.value;
+                var presentUVIndex = document.querySelector("#forecast-present-uv");
+
+                if (uv < 5)
+                {
+                    presentUVIndex.className = "yellow";
+                }
+                else if (uv > 5 && uv <= 7)
+                {
+                    presentUVIndex.className = "orange";
+                }
+                else if (uv > 7 && uv <= 10)
+                {
+                    presentUVIndex.className = "red";
+                }
+                else if(uv > 10)
+                {
+                    presentUVIndex.className = "violet";
+                }
+
+                presentUVIndex.textContent = uv;
+            });
+        }
+        else{
+            alert("Error: " + response.statusText);
+        }
+
+    })
+    .catch(function(error){
+        alert("Unable to retrieve Weather Data for your area at this time");
+    })
+
+}
 searchFormEl.addEventListener("submit", searchHandler);
